cd1ba81801174474a30005d84bce4ff5874c8069
yamaaki <yamaya.akihiro@gmail.com>
2023-05-06 15:10:16 +0900

    x

--- plugins/nvim-cmp.lua
+++ plugins/nvim-cmp.lua
@@ -4,10 +4,8 @@ return {
     -- <C-n> / <C-p> は Copilot の次候補 / 前候補に使う
     -- <C-j> / <C-k> はこれまで通り cmp の選択に使える
     mapping = {
-      -- ["<C-n>"] = function() require("copilot.suggestion").next() end,
-      -- ["<C-p>"] = function() require("copilot.suggestion").prev() end,
-      ["<C-n>"] = function() end,
-      ["<C-p>"] = function() end,
+      ["<C-n>"] = function() require("copilot.suggestion").next() end,
+      ["<C-p>"] = function() require("copilot.suggestion").prev() end,
     },
   }
 }
